151b13e871ced18937a316e5a4c4f6dbe0d7b612
Nepta <nepta23@gmail.com>
2014-07-14 23:06:16 +0200

    use SpriteFactory animation list to create a sprite

--- src/SpriteFactory.cpp
+++ src/SpriteFactory.cpp
@@ -4,6 +4,10 @@ SpriteFactory::SpriteFactory(std::string path = "") : path_(path){}
 
 AnimatedSprite SpriteFactory::create(std::string spriteSheetName){
 	sf::Texture spriteSheet.loadFromFile( path << "/" << spriteSheetName);
+	AnimatedSprite sprite(spriteSheet);
+	for(std::string& animationName : animationList_){
+		sprite.addAnimation(animationName, animationList_[animationName];
+	}
 	return AnimatedSprite(spriteSheet);
 //	initCirno(cirno);
 }
